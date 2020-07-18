bfea4595498e629ddee8075812dde62f68d47c92
dudxor4587 <68459414+dudxor4587@users.noreply.github.com>
2020-07-18 17:01:42 +0900

    10998

---
+++ 김영택/bj.10998/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("");
+		int A = sc.nextInt();
+		System.out.print("");
+		int B = sc.nextInt();
+		
+		System.out.println(A*B);
+	}
+
+}
