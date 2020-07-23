78072f4803dbe3079bc2a3d6ac09427b6c8dd067
dudxor4587 <68459414+dudxor4587@users.noreply.github.com>
2020-07-24 00:41:39 +0900

    15596 modify

--- 김영택/bj.15596/Test.java
+++ 김영택/bj.15596/Test.java
@@ -1,9 +1,9 @@
-
 public class Test {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	long sum(int[] a) {
+		long ans = 0;
+		for(int i = 0; i < a.length; i++) {
+			ans += a[i];
+		}
+		return ans;
 	}
-
 }
