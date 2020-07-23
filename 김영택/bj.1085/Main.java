8bb305a593868dd4abda8efc07626ef712f2b1f4
dudxor4587 <68459414+dudxor4587@users.noreply.github.com>
2020-07-23 16:25:05 +0900

    1085

--- 김영택/bj.1085/Main.java
+++ 김영택/bj.1085/Main.java
@@ -4,28 +4,23 @@ public class Main {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		System.out.print("");
 		int x = sc.nextInt();
-		System.out.print("");
 		int y = sc.nextInt();
-		System.out.print("");
 		int w = sc.nextInt();
-		System.out.print("");
 		int h = sc.nextInt();
 		
-		if(1<= x && x <= w-1 && 1<= y && y<= h-1 && 1<= w && w <= 1000 && 1 <= h && h <= 1000) {
-			int a = x;
-			if (x >= w - x) {
-				a = w - x;
-				if (w-x >= y) {
-					a = y;
-					if (y >= h-y) {
-						a = h-y;
-					}
-				}
-			}
-			System.out.println(a);
+		int a = x;
+		int b = y;
+		if (x >= w - x) {
+			a = w - x;
 		}
+		if (y >= h - y) {
+			b = h - y;
+		}
+		int min = a;
+		if(a >= b) {
+			min = b;
+		}
+		System.out.println(min);
 	}
-
 }
