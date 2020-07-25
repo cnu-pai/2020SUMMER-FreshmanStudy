09ff9c2f40854a7f540731cb9e599fe589ca4045
JongunKim <potionkr@gmail.com>
2020-07-25 10:29:55 +0900

    Add 15596 solution

---
+++ 풀이/week_1/bj15596/Main.java
@@ -0,0 +1,11 @@
+package bj15596;
+
+class Test{
+    long sum(int[] a){
+        long result=0;
+        for(int i=0; i<a.length; i++){
+            result+=a[i];
+        }
+        return result;
+    }
+}
