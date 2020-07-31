f823d785c694072afd8586857f93ae750b14c8c4
dudxor4587 <68459414+dudxor4587@users.noreply.github.com>
2020-07-31 18:46:26 +0900

    3003

---
+++ 김영택/bj.3003/Main.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		int C = sc.nextInt();
+		int D = sc.nextInt();
+		int E = sc.nextInt();
+		int F = sc.nextInt();
+		int a = 1;
+		int b = 1;
+		int c = 2;
+		int d = 2;
+		int e = 2;
+		int f = 8;
+		System.out.print(a - A);
+		System.out.print(" ");
+		System.out.print(b - B);
+		System.out.print(" ");
+		System.out.print(c - C);
+		System.out.print(" ");
+		System.out.print(d - D);
+		System.out.print(" ");
+		System.out.print(e - E);
+		System.out.print(" ");
+		System.out.print(f - F);
+	}
+}
